95b594cca6f49358d33c8717ed2147ea114d18eb
Andrew Hsu <xuzuan@gmail.com>
2022-05-20 17:19:26 -0500

    more

--- .github/workflows/scripts/backport-command/create_milestone.sh
+++ .github/workflows/scripts/backport-command/create_milestone.sh
@@ -4,17 +4,7 @@
 # Discover and create milestone step
 
 if [[ $TARGET_MILESTONE == "auto" ]]; then
-  major=$(echo $BACKPORT_BRANCH | grep -Eo '^v[0-9]{2}\.[0-9]{1,2}\.')
-
-  list_released=$(gh api "repos/redpanda-data/redpanda/releases" --jq '.[] | select(.draft==false).name')
-  latest_released=$(echo "$list_released" | grep -m1 -F "$major" || true)
-  if [[ -z $latest_released ]]; then
-    echo "INFO no previous releases found with major prefix $major."
-    assignee_milestone="${major}1"
-  else
-    echo "INFO found previous releases with major prefix $major."
-    assignee_milestone=$(echo $latest_released | awk -F. -v OFS=. '{$NF += 1; print; exit}')
-  fi
+  assignee_milestone="$BACKPORT_BRANCH-next"
 else
   assignee_milestone=$TARGET_MILESTONE
 fi
